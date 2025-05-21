fcd1a79ae07ca5e61ff7cb9f271f90deaa21b591
dimasdaffa <dimas.de2004@gmail.com>
2025-05-21 10:35:48 +0700

    api response not done yet

---
+++ lib/features/music/data/datasources/song_remote_datasource.dart
@@ -0,0 +1,28 @@
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:mental_health_app/features/music/data/models/SongModel.dart';
+import 'package:http/http.dart' as http;
+
+abstract class SongRemoteDataSource {
+  Future<List<SongModel>> getAllSongs();
+}
+
+class SongRemoteDataSourceImpl implements SongRemoteDataSource {
+  final http.Client client;
+  
+  SongRemoteDataSourceImpl({required this.client});
+  
+  @override
+  Future<List<SongModel>> getAllSongs() async {
+    final response = await client.get(Uri.parse('http://localhost:6000/songs/all'));
+
+    if(response.statusCode == 200) {
+      List jsonResponse = json.decode(response.body);
+      return jsonResponse.map((song) => SongModel.fromJson(song)).toList();
+    } else {
+      throw Exception('Failed to load songs');
+    }
+  }
+  
+}
---
+++ lib/features/music/data/repository/song_repository_impl.dart
@@ -0,0 +1,16 @@
+import 'package:mental_health_app/features/music/data/datasources/song_remote_datasource.dart';
+import 'package:mental_health_app/features/music/domain/entities/song.dart';
+import 'package:mental_health_app/features/music/domain/repository/song_repository.dart';
+
+class SongRepositoryImpl implements SongRepository {
+  final SongRemoteDataSource remoteDataSource;
+
+  SongRepositoryImpl({required this.remoteDataSource});
+
+  @override
+  Future<List<Song>> getAllSongs() async {
+    final songModels = await remoteDataSource.getAllSongs();
+    return songModels;
+  }
+
+}
